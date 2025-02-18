571ee38a1e1e06dc98c5ce46faa9a76cc3cfddb2
Nicolas Berthiaume <n.berthiaume.language.expert@gmail.com>
2025-02-17 20:27:05 -0500

    Added player input logic for choosing board size.

--- battleship.py
+++ battleship.py
@@ -13,19 +13,6 @@ player_big_ship = Ship(4)
 player_med_ship = Ship(3)
 player_small_ship = Ship(2)
 
-boardsize = 5
-
-CPU_board = create_grid(boardsize)
-player_board = create_grid(boardsize)
-
-print("\n")
-print("Welcome to Battleship, Player!")
-print("Here is your board:")
-print("\n")
-
-for row in player_board:
-    print(' '.join(row))
-
 def place_ship(board, ship, start, orientation):
     
     start_row, start_col = start
@@ -63,4 +50,37 @@ def place_ship(board, ship, start, orientation):
     else:
         print("Invalid orientation.")
         return False
+
+size = 5
+board = create_grid(size)
+print(f'creating a {size}x{size} board')
+
+for row in board:
+    print(' '.join(row))
+
+print("\n")
+print("Welcome to Battleship, Player!")
+print("How big do you want the board to be?")
+
+while True:
+    try:
+        boardsize = int(input("Enter a number between 5 and 10: "))
+        if 5 < boardsize < 10:
+            break
+        else:
+            print("Invalid size. Please enter a number better 5 and 10.")
+    except ValueError:
+        ("Invalid input. Please enter a valid integer.")
+
+print("You entered the number " + str(boardsize))
+
+CPU_board = create_grid(boardsize)
+player_board = create_grid(boardsize)
+
+print("\n")
+print("Here is your board:")
+print("\n")
+
+for row in player_board:
+    print(' '.join(row))
 
